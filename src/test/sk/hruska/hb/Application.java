d74e1643d75c336ca5016a91accaec5b381be1cf
mhruska <marek.mhruska@gmail.com>
2018-01-22 11:04:57 +0100

    add method

--- src/test/sk/hruska/hb/Application.java
+++ src/test/sk/hruska/hb/Application.java
@@ -1,5 +1,11 @@
 package sk.hruska.hb;
 
 public class Application {
+ public static void main(String[] args) {
+		System.out.println(process());
+}
 
+private static char[] process() {
+	return null;
+}
 }
