14a8576f558c444da633c8a83f2fc0c9168c4afb
mhruska <marek.mhruska@gmail.com>
2018-01-22 11:06:55 +0100

    first line

--- src/test/sk/hruska/hb/Application.java
+++ src/test/sk/hruska/hb/Application.java
@@ -3,9 +3,14 @@ package sk.hruska.hb;
 public class Application {
  public static void main(String[] args) {
 		System.out.println(process());
+		System.out.println(analyze());
 }
 
 private static char[] process() {
+	return null;
+}
+
+private static char[] analyze() {
 	return null;
 }
 }
